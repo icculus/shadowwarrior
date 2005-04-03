6a8232268cde5e219e5c444453d6d115bf498533
Ryan C. Gordon <icculus@icculus.org>
2005-04-03 02:52:54 +0000

    Valgrind fixes.

--- source/scrip2.c
+++ source/scrip2.c
@@ -205,15 +205,8 @@ void GetToken (BOOL crossline)
 // skip space
 //
 skipspace:
-    while (*script_p <= 32)
+    while ((script_p < scriptend_p) && (*script_p <= 32))
     {
-        if (script_p >= scriptend_p)
-        {
-            if (!crossline)
-                Error ("Line %i is incomplete\n",scriptline);
-            endofscript = TRUE;
-            return;
-        }
         if (*script_p++ == '\n')
         {
             if (!crossline)
